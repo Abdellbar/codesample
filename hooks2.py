6bb9260c440c3f9be4076cec525254516bfd3e73
abdelbar <abdellbar@gmail.com>
2016-10-17 01:20:12 +0200

    Intial commit

--- hooks2.py
+++ hooks2.py
@@ -16,15 +16,17 @@ memegen  = memeapi()
 
 @app.route('/hello',methods=['POST'])
 def parsing():
+   print "got that reqest "
    data = request.json
+   print "parrsed"
    #print format(data['data']['id'])
    msg_id = data['data']['id']
    input_list = sparkbot.get_msg(str(msg_id))
    #print input_list
    #print word.split()[1]
-
+   
    image_file=memegen.get_image(input_list[0],input_list[1],input_list[2])
-   #print  "done"
+   print  "done"
 
    image_name = str(msg_id) +'.jpeg'
    with open(image_name, 'wb') as out_file:
@@ -33,9 +35,9 @@ def parsing():
 
 
    roomId = data['data']['roomId']
-   #print roomId
+   print roomId
    sparkbot.post_msg(str(roomId),"got that")
-   #print "hello"
+   print "hello"
 
    sparkbot.post_file(str(roomId),image_name)
 
@@ -49,6 +51,15 @@ def images(path):
     resp = flask.make_response(open(fullpath).read())
     resp.content_type = "image/jpeg"
     return resp
+
+@app.route("/delete/<path:path>")
+def removefile(path):
+   try:
+       os.remove(path)
+   except Exception as error:
+       app.logger.error("Error removing or closing downloaded file handle", error)
+       print "error removing file"
+   return response
 
 if __name__ == '__main__':
    app.run()
